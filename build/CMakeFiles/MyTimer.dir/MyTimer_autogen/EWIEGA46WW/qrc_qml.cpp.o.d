a2bbe44c78630a57dfa4fca2ac9bab13968d94fa
Oleksandr Yemets <alexxandryemets@gmail.com>
2021-08-16 12:09:15 +0300

    all files deleted from build/ directory

--- build/CMakeFiles/MyTimer.dir/MyTimer_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyTimer.dir/MyTimer_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
- /home/oleksandr/MyTimer/build/MyTimer_autogen/EWIEGA46WW/qrc_qml.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/MyTimer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MyTimer_autogen"
-  "MyTimer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MyTimer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
